ae6979f18e03fa044201178703071aa3b378a9d3
unknown <>
2024-09-22 02:09:56 +0530

    new update

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+const API_KEY = `e4f754914a2fbaaa306ee4bf13d82d74`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    if(document.getElementById('city-name').value.length == 0){
+        alert('Enter a city name')
+    }else{
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+    }
+    
+}
+
+const setInnerText = (id, text) =>{
+    document.getElementById(id).innerText = text;
+}
+
+
+const displayTemperature = temperature => {
+    console.log(temperature);
+    setInnerText('city', temperature.name);
+    setInnerText('temp', temperature.main.temp);
+    setInnerText('weather', temperature.weather[0].main);
+
+    // weather icon settings 
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('image-icon');
+    imgIcon.setAttribute('src', url);
+}
